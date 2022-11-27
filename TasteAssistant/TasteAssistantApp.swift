52f9414442d124936b440c6faf9c2e291e3331b4
ernest0n <magatar136@yandex.ru>
2022-11-27 13:55:02 +0400

    Initial Commit

---
+++ TasteAssistant/TasteAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteAssistantApp.swift
+//  TasteAssistant
+//
+//  Created by Ernest Babayan on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TasteAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
